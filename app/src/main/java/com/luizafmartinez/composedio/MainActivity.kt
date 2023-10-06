b7978e3a73d916063fdcd45ebd20745f4b0c78d5
alxmart <alxmartinez@gmail.com>
2023-10-06 17:21:08 -0300

    Added @Composable       fun DogCard(

--- app/src/main/java/com/luizafmartinez/composedio/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/composedio/MainActivity.kt
@@ -3,9 +3,14 @@ package com.luizafmartinez.composedio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -13,11 +18,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Modifier.Companion.foldIn
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.luizafmartinez.composedio.ui.theme.ComposeDIOTheme
 
@@ -41,7 +50,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Column(modifier = Modifier.background(androidx.compose.ui.graphics.Color.Red)) {
+
+    DogCard(dog = Dog("Luna", "Chow chow"))
+
+
+    /*Column(modifier = Modifier.background(androidx.compose.ui.graphics.Color.Red)) {
         Text(
             text = "Hello World",
             maxLines = 3,
@@ -50,9 +63,10 @@ fun MainScreen() {
         Text(
             text = "Outro texto"
         )
-    }
+    }*/
+
   /*  Button(
-        onClick = { *//*TODO*//* },
+        onClick = { TODO },
         colors = ButtonDefaults.buttonColors(androidx.compose.ui.graphics.Color.Red)
     ) {
         Icon(
@@ -63,6 +77,34 @@ fun MainScreen() {
              color = androidx.compose.ui.graphics.Color.White )
     }*/
 }
+
+data class Dog(
+    val name: String,
+    val breed: String
+)
+
+@Composable
+fun DogCard(dog: Dog) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+        ) {
+        Image(
+            painter = painterResource(id = R.drawable.star_24),
+            contentDescription = null,
+            modifier = Modifier.size(72.dp)
+                .clip(CircleShape)
+        )
+        Column{
+            Text(text = dog.name)
+            Text(text = dog.breed)
+        }
+
+    }
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
